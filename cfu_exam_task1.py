67522b22285a681d2ef35f25aaeaa83c881264a9
Hellidie <Hellidie@users.noreply.github.com>
2020-08-14 18:51:38 +0300

    Create cfu_exam_task1.py

---
+++ cfu_exam_task1.py
@@ -0,0 +1,17 @@
+N = int(input('Введите количество элементов массива: '))
+arr = []
+
+if 1 <= N <= 100:
+    while len(arr) < N:
+        arr.append(int(input('Введите элемент массива:')))
+    for i in arr:
+        if 1 <= int(i) <= 1000:
+            continue
+        else:
+            print('Элементы не должны быть меньше 1 или больше 1000')
+            quit()
+    print(arr)
+else:
+    quit()
+
+print('Максимальный элемент массива: ', max(arr))
