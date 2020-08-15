21b73cee4ad294d6f902360ce702a8e0007d4818
Hellidie <Hellidie@users.noreply.github.com>
2020-08-15 14:10:19 +0300

    final solved

--- cfu_exam_task2.py
+++ cfu_exam_task2.py
@@ -1,3 +1,11 @@
 string = input('Введите слова через пробел: ')
 wordList = string.split()
+count = 0
+
 print('Количество слов: ', len(wordList))
+
+for word in wordList:
+    if word.find('r') != -1:
+        count += 1
+
+print('Слов с буквой 'r':', count)
