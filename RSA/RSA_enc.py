15dfc267d2d651c598063981e51c26ce94abcc15
Hellidie <Hellidie@users.noreply.github.com>
2020-09-05 18:24:14 +0300

    Create RSA_enc.py

---
+++ RSA/RSA_enc.py
@@ -0,0 +1,28 @@
+from Crypto.Util.number import *    # pip install pycryptodome
+
+p = getPrime(64)                    # getPrime(N) генерирует случайное простое число длиной N бит
+q = getPrime(64)
+e = 65537
+
+n = p * q
+f = (p - 1) * (q - 1)
+d = inverse(e, f)
+
+dict_open = {                       # открытый ключ
+    "e": e,
+    "n": n
+}
+
+dict_closed = {                     # закрытый ключ
+    "d": d,
+    "n": n
+}
+
+print(dict_open, dict_closed)
+
+flag = bytes("flag{i_love_tk_ctf}", encoding='utf-8')
+message = bytes_to_long(flag)
+c = (message ** e) % n              # шифруем сообщение
+print(c)
+
+# формула для расшифровки: message = c ** d % n
